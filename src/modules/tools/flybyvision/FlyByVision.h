b9c8480b469136d1ffbf7512c535e92050efecc8
Nelio Oliveira <nelio.smix@gmail.com>
2026-09-01 06:11:17 +0100

    flyby: align runtime state with protocol

--- src/modules/tools/flybyvision/FlyByVision.h
+++ src/modules/tools/flybyvision/FlyByVision.h
@@ -3,6 +3,7 @@
 #include "libs/Module.h"
 #include "Pin.h"
 #include "FlyByTrigger.h"
+#include "FlyByProtocol.h"
 
 class Gcode;
 
@@ -18,18 +19,26 @@ public:
     static void service_pulses_from_isr();
 
     bool consume_pending(FlyByTrigger& trigger, float block_mm);
+    static void finalize_trigger_tick(FlyByTrigger& trigger, float block_mm,
+                                      float entry_speed, float exit_speed,
+                                      float maximum_rate_steps_s, uint32_t steps_event_count,
+                                      uint32_t accelerate_until, uint32_t decelerate_after,
+                                      uint32_t total_move_ticks, float tick_frequency);
 
 private:
     static FlyByVision *instance;
 
     void load_config();
+    void set_mode(Gcode *gcode);
     void arm(Gcode *gcode);
+    void set_timing(Gcode *gcode);
     void cancel();
     void status(Gcode *gcode);
 
     Pin camera_pin;
     Pin strobe_pin;
     bool enabled{false};
+    FlyByProtocol::Mode mode{FlyByProtocol::AUTO};
     uint32_t camera_width_ticks{4};
     uint32_t strobe_width_ticks{8};
 
